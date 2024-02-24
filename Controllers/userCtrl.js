52384437be05e208def75c4a884adad2cfb4aa4e
ufy <ufuomaitwa@gmail.com>
2024-02-24 15:00:18 +0100

    edits

--- Controllers/userCtrl.js
+++ Controllers/userCtrl.js
@@ -73,7 +73,7 @@ module.exports.login_post = async (req, res) => {
     }
     catch(err) {
         const errors = handleErrors(err)
-        res.status(400).json({ errors })
+        res.status(400).send({ errors })
     }
 }
 
