6094afee6eaaae9a8cfc932ee13677e4c48b726b
ufy <ufuomaitwa@gmail.com>
2024-02-25 00:44:59 +0100

    changes to authMiddleware

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const config = require('../config/default')
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
-    if (!token) return res.status(401).json({ message: err.message });
+    if (!token) return res.status(401).json({ message: 'Unauthorized' });
     try {
         const decoded = jwt.verify(token, config.jwtKey)
         req.user = decoded;
