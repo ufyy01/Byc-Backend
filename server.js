ec1c05145a5d7d82af5dd1a17a2d29ce11231371
ufy <ufuomaitwa@gmail.com>
2024-02-24 17:43:48 +0100

    MORE CORS EDITS

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://127.0.0.1:5500"
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "DELETE"]
 }))
 // app.use(express.urlencoded({extended: true}))
 
