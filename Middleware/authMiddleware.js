28e9e89a1e5d440ae44621fbbe2d9ee92b8ccd55
ufy <ufuomaitwa@gmail.com>
2024-02-24 12:54:09 +0100

    edit

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const config = require('../config/default')
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
-    if (!token) return res.status(401).send('Acess denied. No token provided');
+    if (!token) return res.status(401).send('Access denied. No token provided');
     try {
         const decoded = jwt.verify(token, config.jwtKey)
         req.user = decoded;
