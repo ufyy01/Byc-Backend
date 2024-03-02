80ad324de7d9c32dc37c2cfec957d3b3630fd29b
ufy <ufuomaitwa@gmail.com>
2024-03-02 02:27:56 +0100

    edits to userCtrl and authMiddleware

--- Controllers/userCtrl.js
+++ Controllers/userCtrl.js
@@ -63,6 +63,7 @@ module.exports.login_post = async (req, res) => {
         const token = user.generateAuthToken()
 
         res.status(200)
+        .header('x-auth-token', token)
         .header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500') 
         .header('Access-Control-Allow-Credentials', 'true')
         .json({ token, user: user.name });
